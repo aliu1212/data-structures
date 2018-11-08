5db9b5f596958889a4e4439cd3cedaca50590db7
alex <alexliu12@gmail.com>
2018-11-07 17:15:08 -0800

    finished es6

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,24 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.tail = 0;
+    this.head = 1;
+  }
+
+  enqueue(value) {
+    this.tail++;
+    this[this.tail] = value;
+  }
+  
+  dequeue() {
+    var temp = this[this.head];
+    delete this[this.head];
+    this.head++;
+    return temp;
+  }
+  
+  size() {
+    return Math.max(0, (this.tail-this.head + 1));
   }
 
 }
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,25 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.index = 0;
+  }
+
+  push(value) {
+    this.index += 1;
+    this[this.index] = value;
+  }
+
+  pop() {
+    var temp = this[this.index];
+    delete this[this.index];
+    if (this.index > 0) {
+      this.index--;
+    }
+    return temp;
+  }
+
+  size() {
+    return this.index;
   }
 
 }
