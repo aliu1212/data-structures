9b2b5de2ce9a5ec2d11e0f4faa573c427584b788
alex <alexliu12@gmail.com>
2018-11-07 16:22:14 -0800

    completed prototypal

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,28 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var stacky = Object.create(queueMethods);
+  stacky.tail = 0;
+  stacky.head = 1;
+  return stacky;
 };
 
 var queueMethods = {};
 
+
+queueMethods.enqueue = function(value) {
+  this.tail++;
+  this[this.tail] = value;
+};
+
+queueMethods.dequeue = function() {
+  var temp = this[this.head];
+  delete this[this.head];
+  this.head++;
+  return temp;
+};
+
+queueMethods.size = function() {
+  return Math.max(0, (this.tail-this.head + 1));
+};
 
--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,27 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var stacky = Object.create(stackMethods);
+  stacky.index = 0;
+  return stacky;
 };
 
 var stackMethods = {};
 
+stackMethods.push = function(value) {
+  this.index += 1;
+  this[this.index] = value;
+};
 
+stackMethods.pop = function() {
+  var temp = this[this.index];
+  delete this[this.index];
+  if (this.index > 0) {
+    this.index--;
+  }
+  return temp;
+};
+
+stackMethods.size = function() {
+  return this.index;
+};
