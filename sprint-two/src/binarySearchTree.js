a1c359540d858ce953cf962386e4deab1e7060cf
aliu1212 <alexliu12@gmail.com>
2018-11-10 12:24:25 -0800

    finished binary tree!

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,19 +1,66 @@
 var BinarySearchTree = function(value) {
-  this.value = value;
+  var newbsTree = Object.create(bsMethods);
+  //this.bsTree = {};
+  newbsTree.value = value;
+  newbsTree.left = null;
+  newbsTree.right = null;
+  return newbsTree;
 };
 
+var bsMethods = {};
 
-BinarySearchTree.prototype.insert = function(value) {
+bsMethods.insert = function(val) {
+  if (val > this.value) {
+    if (this.right === null) {
+      this.right = BinarySearchTree(val);
+    } else {
+      this.right.insert(val);
+    }
+  } else if (val < this.value) {
+    if (this.left === null) {
+      this.left = BinarySearchTree(val);
+    } else {
+      this.left.insert(val);
+    }
+  }
+};
+
+bsMethods.contains = function(val) {
+
+  if (this.value === val) {
+    return true;
+  }
+
+  let leftHas = false;
+  let rightHas = false;
+
+  if (this.left !== null) {
+    leftHas = this.left.contains(val);
+  } 
+  if (this.right !== null) {
+    rightHas = this.right.contains(val);
+  } 
+  if (leftHas || rightHas) {
+    return true;
+  }
+  
+  return false;
 
 };
 
-BinarySearchTree.prototype.contains = function() {
+bsMethods.depthFirstLog = function(cb) {
+
+  cb(this.value);
+    
+  if (this.left !== null) {
+    this.left.depthFirstLog(cb);
+  } 
+  if (this.right !== null) {
+    this.right.depthFirstLog(cb);
+  } 
 
 };
 
-BinarySearchTree.prototype.depthFirstLog = function() {
-
-};
 /*
  * Complexity: What is the time complexity of the above functions?
  */
